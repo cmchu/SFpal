07f365a11dd5d8e2bd539c07cfff136bc7a7c214
evelynpeng <yhpeng11@gmail.com>
2017-04-20 12:25:02 -0700

    add checkbox

--- functions/app/app_server_template.py
+++ functions/app/app_server_template.py
@@ -1,16 +1,20 @@
-from flask import Flask, request, jsonify,render_template
-
+from flask import Flask, request, jsonify,render_template, redirect
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def default():
 
     #output = dict()
 
     # this could be a web page of docs instead
     #output['message'] = 'Welcome to the SFpal!'
+    if request.method == "POST":
+        if request.form['submit'] == 'submit':
+            selected = request.form.getlist('check')
+            any_selected = bool(selected)
+            print selected
     return render_template('mainpage.html')
     #return script
    
@@ -21,6 +25,25 @@ def post_history():
     out = request.args.to_dict()
 
     return jsonify(out)
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+
+    test = request.form.getlist('checks')
+    print test
+    return jsonify(test)
+   #else:
+    #   return redirect('/')
+
+@app.route('/test2', methods=['GET', 'POST'])
+def test2():
+
+    if request.method == "POST":
+        if request.form['submit'] == 'submit':
+            print(request.args.get('check'))
+
+    return render_template('test.html')
+
 
 if __name__ == "__main__":
     app.debug = True # only have this on for debugging!
