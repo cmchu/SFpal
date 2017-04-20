0fabf1d680a4a1005db099a0c735fb9227ab7172
linda liu <tliu44@dons.usfca.edu>
2017-04-20 14:14:34 -0700

    add redirect page

--- functions/app/app_server_template.py
+++ functions/app/app_server_template.py
@@ -1,6 +1,18 @@
-from flask import Flask, request, jsonify,render_template, redirect
+from flask import Flask, request, jsonify,render_template, redirect, url_for
+from sklearn.metrics import jaccard_similarity_score
+
+import pandas as pd
 
 app = Flask(__name__)
+
+
+def predict(input):
+
+    for row in range(gold_standard.shape[0]):
+        similarity = jaccard_similarity_score(gold_standard.drop(["similarity", "zip"], axis=1).ix[row,],input)
+        gold_standard.ix[row, "similarity"] = similarity
+
+    return gold_standard.sort_values("similarity", ascending=False).reset_index(drop=True).ix[[0, 1, 2], "zip"]
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -12,39 +24,41 @@ def default():
     #output['message'] = 'Welcome to the SFpal!'
     if request.method == "POST":
         if request.form['submit'] == 'submit':
-            selected = request.form.getlist('check')
-            any_selected = bool(selected)
-            print selected
+            selected_val = ','.join(request.form.getlist('check'))
+            return redirect(url_for('.do_result', selected_val=selected_val))
     return render_template('mainpage.html')
-    #return script
+
+
+@app.route('/results')
+def do_result():
+    selected_val = request.args['selected_val']
+    # replace this with a query from whatever database you're using
+    val = selected_val.split(",")
+
+    return get_community(val)
+
    
 @app.route('/Ready')
 def post_history():
 
-    #return a json to see all the variable inputs
+    # return a json to see all the variable inputs
     out = request.args.to_dict()
 
     return jsonify(out)
 
-@app.route('/result',methods = ['POST', 'GET'])
-def result():
 
-    test = request.form.getlist('checks')
-    print test
-    return jsonify(test)
-   #else:
-    #   return redirect('/')
+@app.route('/SFpal')
+def get_community(selected):
 
-@app.route('/test2', methods=['GET', 'POST'])
-def test2():
-
-    if request.method == "POST":
-        if request.form['submit'] == 'submit':
-            print(request.args.get('check'))
-
-    return render_template('test.html')
+    keys = ['crime', 'sanitation', 'noise-free', 'attractiveness', 'child-friendly', 'greenery', 'walking-friendly',
+            'restaurant', 'coffee-lover', 'night-life', 'dog-person', 'construction']
+    vals = [1.0 if i in selected else 0.0 for i in keys]
+    prediction = predict(vals)
+    out = zip(['Best', 'Better Than Good', 'Good'], prediction)
+    return jsonify(out)
 
 
 if __name__ == "__main__":
-    app.debug = True # only have this on for debugging!
-    app.run(host='0.0.0.0') # need this to access from the outside world!
+
+    gold_standard = pd.read_csv('gold_standard.csv')
+    app.run(host='0.0.0.0')# need this to access from the outside world!
