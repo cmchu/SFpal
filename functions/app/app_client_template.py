91907f2894cf5eb1304b151c5ef9e700e335a939
lindarliu <tliu44@dons.usfca.edu>
2017-04-26 09:54:59 -0700

    turn it in on canvas

--- functions/app/app_client_template.py
+++ functions/app/app_client_template.py
@@ -1,6 +1,6 @@
 import httplib
 from bs4 import BeautifulSoup
-
+import urllib2
 
 # project name: SFpal
 
@@ -8,28 +8,24 @@ from bs4 import BeautifulSoup
 
 
 # use this server for dev
-SERVER = '0.0.0.0:5000'
+SERVER = 'ec2-54-186-133-112.us-west-2.compute.amazonaws.com/'
 
 # use this server for prod, once it's on ec2
 # SERVER = 'xxxxx.aws.ec2.com:5000'
 def get_default():
-    out = dict()
-    h = httplib.HTTPConnection(SERVER)
-    h.request('GET', 'http://'+SERVER+'/')
-    resp = h.getresponse()
-    out = resp.read()
-    return out
+
+    URL = 'http://'+SERVER+''
+    print URL
+    req = urllib2.Request(URL, headers={'User-Agent2': "Resistance is futile"})
+    out = urllib2.urlopen(req)
+
+    return out.read()
 
 def get_result():
     keys = ["sanitation", 'coffee', "construction"]
-
-    h = httplib.HTTPConnection(SERVER)
-
-    # want the url to look like this
-    # http://0.0.0.0:5000/result?selected_val=sanitation%2Ccoffee%2Cconstruction#something
-    h.request('GET', 'http://'+SERVER+'/result?selected_val=' + "%2C".join(keys)+'#something')
-    resp = h.getresponse()
-    out = resp.read()
+    URL = 'http://'+SERVER+'/result?selected_val=' + "%2C".join(keys)+'#something'
+    req = urllib2.Request(URL, headers={'User-Agent': "Resistance is futile"})
+    out = urllib2.urlopen(req)
     soup = BeautifulSoup(out, 'html.parser')
     result = soup.find("section", {"id": "services"})
 
