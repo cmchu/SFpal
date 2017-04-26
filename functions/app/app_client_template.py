a796779956ca60ebfcebba0701d1393a2fba9386
lindarliu <tliu44@dons.usfca.edu>
2017-04-25 20:42:53 -0700

    meet miky's standard

--- functions/app/app_client_template.py
+++ functions/app/app_client_template.py
@@ -2,12 +2,23 @@ import httplib
 from bs4 import BeautifulSoup
 
 
+# project name: SFpal
+
+# authors are Christine Chu, Linda Liu, Evelyn Peng
+
+
 # use this server for dev
 SERVER = '0.0.0.0:5000'
 
 # use this server for prod, once it's on ec2
 # SERVER = 'xxxxx.aws.ec2.com:5000'
-
+def get_default():
+    out = dict()
+    h = httplib.HTTPConnection(SERVER)
+    h.request('GET', 'http://'+SERVER+'/')
+    resp = h.getresponse()
+    out = resp.read()
+    return out
 
 def get_result():
     keys = ["sanitation", 'coffee', "construction"]
@@ -31,4 +42,10 @@ if __name__ == '__main__':
     print "created by  Christine Chu, Linda Liu, Evelyn Peng"
     print "************************************************"
     print "******** SFpal class test **********"
+    print "Here we enter three things that we want our" \
+          " community to look like: 'sanitation', 'coffee', 'construction'"
+    print "The app will deliver three zipcodes that best meet our requirement:"
     print get_result()
+    print "************************************************"
+    print "******** SFpal default **********"
+    print get_default()
